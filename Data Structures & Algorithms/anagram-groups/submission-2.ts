933efb3d03a3a2ca005be12cf8cffe0e75101d24
Igor <102683696+IgorPanasik@users.noreply.github.com>
2026-05-14 23:46:47 +0300

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.ts
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs: string[]): string[][] {
+        const map: Map<string, string[]> = new Map();
+
+        for(const word of strs) {
+            const count = new Array(26).fill(0);
+            for(const char of word) {
+                count[char.charCodeAt(0) - 97]++;
+            }
+            const key = count.join(',');
+            const group = map.get(key) || [];
+            group.push(word);
+            map.set(key, group);
+        }
+        return [...map.values()];
+    }
+}
