d6370e6e37841fb70bf126d5b088a58d82f08031
Igor <102683696+IgorPanasik@users.noreply.github.com>
2026-05-19 01:42:00 +0300

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.ts
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums: number[]): number[] {
+        const res: number[] = [];
+        let product = 1;
+
+        for(let i = 0; i < nums.length; i++) {
+            res[i] = product;
+            product *= nums[i];
+        }
+  
+        product = 1;
+        for(let i = nums.length - 1; i >= 0; i--) {
+            res[i] = res[i] * product;
+            product *= nums[i];
+        }
+        return res;
+    }
+}
